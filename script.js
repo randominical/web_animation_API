e1b4a19e5e5305f1da69609d560e26b89d0278f5
Anna Agafonova <aa.agafonova@gmail.com>
2021-03-01 13:35:45 +0300

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+const ballElement = document.querySelector('.ball');
+
+const keyframes = [
+    {left: '50%', top: '0', backgroundColor: 'red'},
+    {left: '250px', top: '50%'},
+    {left: '50%', top: '250px', backgroundColor: 'blue'},
+    {left: '0', top: '50%'},
+    {left: '50%', top: '0', backgroundColor: 'red'},
+]
+
+const timingEffect = {
+    duration: 2000,
+    iterations: Infinity,
+}
+
+const animation = ballElement.animate(keyframes, timingEffect)
